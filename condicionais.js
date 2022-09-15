09a90c832d425b6c73d6b863ebf48a5165b42132
Letícia Coelho <90153628+coelholeticia@users.noreply.github.com>
2022-09-15 15:58:02 -0300

    Add files via upload

--- condicionais.js
+++ condicionais.js
@@ -1,3 +1,84 @@
+<<<<<<< HEAD
+// exercicio pizzaria
+
+/*let SaborPizza = prompt("Qual sabor você quer ?");
+
+if(SaborPizza == "marguerita"){
+    console.log("Preparar pizza de marguerita");
+}else if(SaborPizza == "quatro-queijos"){
+    console.log("Preparar pizza de quatro queijos");
+}else if(SaborPizza == "calabresa"){
+    console.log("Preparar pizza de calabresa");
+}else{
+    console.log("Não temos essa pizza");
+}
+*/
+
+//função exercicio 2:
+
+function verificarPermissoes (){
+    let idade = prompt("Qual a sua idade?");
+    
+    if(idade >= 16 && idade < 18){
+        console.log("você pode votar, mas ainda não pode dirigir");
+    }else if( idade >= 18){
+        console.log("você pode dirigir e votar");
+    }else{
+        console.log("Você ainda não possui idade suficiente para votar ou dirigir");
+    }
+
+
+}
+
+//verificarPermissoes();
+
+// com retorno 
+
+function previsaoRodagem(qntGasolina, quilometragem){
+ 
+    return  qntGasolina / quilometragem;
+}
+
+//console.log(previsaoRodagem(3000, 500));
+
+//sem retorno
+
+function previsaoRodagem(qntGasolina, quilometragem){
+ 
+    console.log (qntGasolina / quilometragem);
+}
+
+//previsaoRodagem(3000, 500);
+
+
+//calcular imc 
+
+function verificarIMC() {
+    let altura = prompt("qual a sua altura?");
+    let peso = prompt("qual o seu peso ?");
+
+    let imc =  peso / (altura * altura);
+
+    if (imc < 17) {
+      console.log("Muito abaixo do peso");
+    } else if (imc > 17 && imc <= 18.49){
+      console.log("Abaixo do peso");
+    } else if (imc >= 18.5 && imc <= 24.99){
+      console.log("Peso normal");
+    } else if (imc >= 25 && imc <= 29.99){
+      console.log("Acima do peso");
+    } else if (imc >= 30 && imc <= 34.99){
+      console.log("Obesidade I");
+    } else {
+      console.log("Obesidade II");
+    }
+  }
+
+  verificarIMC();
+
+
+
+=======
 // exercicio pizzaria
 
 /*let SaborPizza = prompt("Qual sabor você quer ?");
@@ -59,3 +140,4 @@ function verificarIMC() {
 
 
 
+>>>>>>> d91a18a2287acfebb177a2c2f5deec58ed2d6771
